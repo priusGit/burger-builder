5069626ac813b4f8edc1ac17a8a62e26da333cd2
priusGit <mrszaro2000@wp.pl>
2020-07-26 12:29:53 +0200

    halfsec8 beggining of the burger

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
-    <div><Layout><p>Test</p></Layout></div>
+    <div>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
+    </div>
   );
 }
 
