0935232a33f60098c106aeabd920698404829447
priusGit <mrszaro2000@wp.pl>
2020-07-28 22:05:20 +0200

    sec8 50%

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,14 +6,14 @@ const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey + 1} type={igKey} />
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
         })
         .reduce((arr, el) => {
             return arr.concat(el)
         }, []);
     if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>
+        transformedIngredients = <p>Please start adding ingredients!</p>;
     }
     return (
         <div className={classes.Burger}>
@@ -24,4 +24,4 @@ const burger = (props) => {
     );
 };
 
-export default burger
+export default burger;
